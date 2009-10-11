b65739fa3c59fcd178e3cc0384369df027551865
Jakub Wilk <jwilk@jwilk.net>
2009-10-11 17:26:13 +0200

    Add support for Linux input subsystem.

--- keyboardleds.py
+++ keyboardleds.py
@@ -7,15 +7,39 @@ if not _p_linux:
 
 if _p_linux:
 
+    import time
     import fcntl
     import os
+    import stat
     import struct
 
     _KDGETLED = 0x4B31
     _KDSETLED = 0x4B32
-    _LED_SCR = 1
-    _LED_NUM = 2
-    _LED_CAP = 4
+
+    _tty_leds = dict(
+        scroll_lock = 0x01,
+        num_lcok = 0x02,
+        caps_lock = 0x04,
+    )
+
+    _EV_LED = 0x11
+
+    _input_leds = dict(
+        num_lock = 0x00,
+        caps_lock = 0x01,
+        scroll_lock = 0x02,
+        compose = 0x03,
+        kana = 0x04,
+        sleep = 0x05,
+        suspend = 0x06,
+        mute = 0x07,
+        misc = 0x08,
+        mail = 0x09,
+        charging = 0x0a,
+    )
+
+    _EVENT_DEV_MIN = 0x0D40
+    _EVENT_DEV_MAX = _EVENT_DEV_MIN + 0x20 - 1
 
 _MAGIC = []
 
@@ -26,10 +50,17 @@ class LedKit(object):
         def __init__(self, filename):
             self._filename = filename
             self._fd = os.open(filename, os.O_WRONLY)
+            info = os.fstat(self._fd)
+            self._input_subsystem = stat.S_ISCHR(info.st_mode) and _EVENT_DEV_MIN <= info.st_rdev <= _EVENT_DEV_MAX
             self._leds = {}
-            Led(self, 'caps_lock', _LED_CAP, magic=_MAGIC)
-            Led(self, 'scroll_lock', _LED_SCR, magic=_MAGIC)
-            Led(self, 'num_lock', _LED_NUM, magic=_MAGIC)
+            if self._input_subsystem:
+                self.get = self._get_standalone
+                self.set = self._set_standalone
+                for name, n in _input_leds.iteritems():
+                    InputEventLed(self, name, n, magic=_MAGIC)
+            else:
+                for name, n in _tty_leds.iteritems():
+                    Led(self, name, n, magic=_MAGIC)
 
         def __del__(self):
             try:
@@ -41,6 +72,8 @@ class LedKit(object):
             fcntl.ioctl(self._fd, _KDSETLED, n)
 
         def _get(self):
+            if self._input_subsystem:
+                raise NotImplementedError
             bytes = struct.pack('I', 0)
             bytes = fcntl.ioctl(self._fd, _KDGETLED, bytes)
             [result] = struct.unpack('I', bytes)
@@ -51,6 +84,14 @@ class LedKit(object):
         for light in lights:
             n |= light._n
         self._set(n)
+
+    def _set_standalone(self, lights):
+        lights = set(lights)
+        offs = set(self._leds.values()) - lights
+        for led in offs:
+            led.reset()
+        for led in lights:
+            led.set()
 
     def get(self):
         n = self._get()
@@ -65,10 +106,15 @@ class LedKit(object):
             i *= 2
         return result
 
+    def _get_standalone(self):
+        return [led for led in self._leds.itervalues() if led.get()]
+
     def __repr__(self):
         return '%s.%s(%r)' % (self.__class__.__module__, self.__class__.__name__, self._filename)
 
 class Led(object):
+
+    standalone = False
 
     def __init__(self, control, name, n, magic=None):
         if magic is not _MAGIC:
@@ -97,5 +143,30 @@ class Led(object):
 
     def __repr__(self):
         return '%s.%s' % (self._control, self._name)
+
+if _p_linux:
+
+    class InputEventLed(Led):
+
+        standalone = True
+
+        def _set(self, value):
+            now = time.time()
+            sec = int(now)
+            usec = int((now - sec) * 1.0E6)
+            data = struct.pack('@llHHI', sec, usec, _EV_LED, self._n, value)
+            os.write(self._control._fd, data)
+
+        def set(self):
+            self._set(1)
+
+        def toggle(self):
+            raise NotImplementedError
+
+        def reset(self):
+            self._set(0)
+
+        def get(self):
+            raise NotImplementedError
 
 # vim:ts=4 sw=4 et

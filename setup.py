0ea720bf10ef3cea2152489b19d4d576f48a293d
Jakub Wilk <jwilk@jwilk.net>
2017-04-19 19:24:54 +0200

    setup: use distutils644.

--- setup.py
+++ setup.py
@@ -36,6 +36,13 @@ try:
 except ImportError:
     sphinx_setup_command = None
 
+try:
+    import distutils644
+except ImportError:
+    pass
+else:
+    distutils644.install()
+
 b''  # Python >= 2.6 is required
 
 def get_version():
